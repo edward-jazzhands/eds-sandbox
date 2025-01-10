db7e87584404232a4f8b19aa9b88be1bfcea9ecd
edward-jazzhands <ed.jazzhands@gmail.com>
2025-01-10 08:33:15 -0400

    new file:   python/textual/examples/app_loading.py new file:   python/textual/examples/loading_with_spinner.py

---
+++ python/textual/examples/app_loading.py
@@ -0,0 +1,36 @@
+
+from textual.app import App
+from textual import work
+from textual.widget import Widget
+from textual.widgets import Header, Footer, Label
+import asyncio
+
+class MyWidget(Widget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loading = True
+
+    def compose(self):
+        yield Label("Hello world.")
+
+    def on_mount(self):
+        self.call_after_refresh(self.init)
+
+    @work
+    async def init(self):
+        await asyncio.sleep(3)
+        self.loading = False
+
+class MyApp(App):
+
+    DEFAULT_CSS = """MyWidget {
+    width: 1fr; height: 1fr; content-align: center middle; align: center middle;
+    }"""
+
+    def compose(self):
+        yield Header()
+        yield MyWidget()
+        yield Footer()
+
+MyApp().run()
