8ea0d7882721b25e89183658e37628300405b289
Edward Jazzhands <ed.jazzhands@gmail.com>
2025-07-09 01:16:33 -0300

    modified:   python/textual/examples/textarea_submit.py

--- python/textual/examples/textarea_submit.py
+++ python/textual/examples/textarea_submit.py
@@ -1,37 +1,46 @@
 # This shows how to add a Submitted action to a TextArea widget which
 # replicates the Submitted messge in the Input widget, and which can
-# be triggered by pressing a key (Enter key in this case).
+# be triggered by pressing a key or combo (ctrl+s in this case).
 
 from __future__ import annotations
 from textual import on
+from textual.app import App
 from textual.message import Message
 from textual.binding import Binding
-from textual.widgets import TextArea
-from textual.widget import Widget
+from textual.widgets import TextArea, Footer
 
 class MyTextArea(TextArea):
 
     class Submitted(Message):
         def __init__(self, textarea: MyTextArea, text: str):
-            self.textarea = textarea    # the MyTextArea widget that sent the message
+            super().__init__()
+            self.textarea = textarea  
             self.text = text
-            self.value = text  # extra alias for text
     
     BINDINGS = [
-        Binding("enter", "submit", "Submit", show=True),
+        Binding("ctrl+s", "submit", "Submit", show=True),
     ]
     
     def action_submit(self):
         self.post_message(self.Submitted(self, self.text))
 
 
-# USAGE IN PARENT:
-class MyParentFoo(Widget):
+
+class TextualApp(App[None]):
+
+    CSS = """
+    Screen { align: center middle; }
+    MyTextArea { border: solid blue; width: 50%; height: 50%; }
+    """
 
     def compose(self):
         yield MyTextArea()
+        yield Footer()
     
     @on(MyTextArea.Submitted)
     def my_textarea_submitted(self, event: MyTextArea.Submitted):
-        self.log.info(f"TextArea submitted with text: {event.text}")
+        self.notify(f"TextArea submitted with text: {event.text}")
         # handle submit here
+
+
+TextualApp().run()
